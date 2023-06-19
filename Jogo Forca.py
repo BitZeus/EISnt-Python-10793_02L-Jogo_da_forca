36f84697ac1129e500724361c620c008abbe410f
AndreiaMa <andreiasrmar@gmail.com>
2023-06-20 00:08:45 +0200

    Update
    Adicionado o numero maximo de tentativas para o utilizador.

--- Jogo Forca.py
+++ Jogo Forca.py
@@ -176,39 +176,40 @@ def desenhar_boneco(tentativas):
 def jogo_forca():
     tentativas = 0
     letras_erradas = []
-    #letras_adivinhadas = palavra_secreta[] #Acrescentei dicionário letras corretas
+    #letras_adivinhadas = palavra_secreta[] 
 
     print("Olá, bem-vindo ao jogo da Forca!")          
     print("Adivinha a palavra secreta.")
     print("Vamos dar-te uma dica, é uma das freguesias de Lisboa!")
-
+    print("Tens 5 tentativas. Let's play :)")
+    print("**********************JOGO DA FORCA**************************")
 
     while tentativas < max_tentativas:
-        countprint=0    #contador para nr de impressoes no caso de vitoria
+        countprint=0    # Contador para nr de impressoes no caso de vitoria
         letra = input("Digita uma letra: ").lower() #solicita uma letra ao jogador e converte para minuscula
 
-        if letra in alfabeto and len(letra) == 1: #verifica se o caractere existe na lista albeto[] e se é um unico caratere
-            if letra in letras_adivinhadas or letra in letras_erradas: #Impedir a repetição de letras.
+        if letra in alfabeto and len(letra) == 1: # Verifica se o caractere existe na lista albeto[] e se é um unico caratere
+            if letra in letras_adivinhadas or letra in letras_erradas: # Impedir a repetição de letras.
                 print("Já digistaste essa letra, tenta outra!")
                 continue
             
             if letra in palavra_secreta:            # Verifica se a letra fornecida está presente na palavra secreta
                 for i in range(len(palavra_secreta)):
                     if palavra_secreta[i] == letra:
-                        if i==0:  #Assume/coloca o primeira letra sempre como maiscula
+                        if i==0:  # Assume/coloca o primeira letra sempre como maiscula
                             letras_adivinhadas[i] = letra.upper()
                         else:
-                            letras_adivinhadas[i] = letra #restantes letras como minusculas
-                        if countprint<1: #se contador menor 1 imprime mensagem de vitória
-                            print("Parabéns, acertaste numa letra!") #acrescentei Parabéns acertaste, na letra.
+                            letras_adivinhadas[i] = letra # Restantes letras como minusculas
+                        if countprint<1: # Se contador menor 1 imprime mensagem de vitória
+                            print("Parabéns, acertaste numa letra!") 
                             countprint+=1
                         else:           
                             continue
             else:
-                letras_erradas.append(letra)        #adiciona a letra errada à lista letras_erradas[]
-                tentativas += 1                     #incrementa o numero de tentativas já usadas
-                desenhar_boneco(tentativas)         #Exibe o boneco de acordo com o nº de tentativas
-                print(f"Letra Errada. Numero de tentativas restantes: {max_tentativas-tentativas}.") #apresenta o numero de tentaivas restantes.
+                letras_erradas.append(letra)        # Adiciona a letra errada à lista letras_erradas[]
+                tentativas += 1                     # Incrementa o numero de tentativas já usadas
+                desenhar_boneco(tentativas)         # Exibe o boneco de acordo com o nº de tentativas
+                print(f"Letra Errada. Numero de tentativas restantes: {max_tentativas-tentativas}.") # Apresenta o numero de tentaivas restantes.
                    
             # Atualizar a exibição da palavra com a letra revelada, se estiver correta
             print("Palavra: ", " ".join(letras_adivinhadas))
@@ -217,14 +218,14 @@ def jogo_forca():
             if "_" not in letras_adivinhadas:
                 print("Parabéns!! Ganhaste \o/ \o/")
                 return
-        elif len(letra) == 1:                       #faz o print de erro se for um único caractere mas que não está na lista alfabeto[], ou seja, numeros
+        elif len(letra) == 1:                       # Faz o print de erro se for um único caractere mas que não está na lista alfabeto[], ou seja, numeros
             print("Inválido. Apenas letras. Tenta novamente.")
-        else:                                       #faz o print de erro se nenhuma das opções
+        else:                                       # Faz o print de erro se nenhuma das opções
             print("Inválido. Digita uma única letra por favor.")
 
-        print("Letras erradas: ", ", ".join(letras_erradas)) #Apresenta sempre ao jogador as letras erradas
+        print("Letras erradas: ", ", ".join(letras_erradas)) # Apresenta sempre ao jogador as letras erradas
 
-    print("Oh, não conseguiste! A palavra secreta era:", palavra_secreta,".Tenta outra vez!") #Mensagem de fim jogo por atingir o numero maximo de tentativas sem sucesso.
+    print("Oh, não conseguiste! A palavra secreta era:", palavra_secreta,".Tenta outra vez!") # Mensagem de fim de jogo por atingir o numero maximo de tentativas sem sucesso.
 
 # Iniciar o jogo da forca
-jogo_forca()    #inicia a função principal do jogo
+jogo_forca()    # Inicia a função principal do jogo
